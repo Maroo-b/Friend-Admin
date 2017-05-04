3272918eba9197fe6eae04368652c1897f85fb8d
Marouen Bousnina <marouen.it.technology@gmail.com>
2017-05-04 16:14:22 +0200

    Initial commit
    Setup project and configure required gems

---
+++ config/application.rb
@@ -0,0 +1,29 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module FriendAdmin
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.assets.quiet = true
+
+    config.generators do |generate|
+      generate.helper false
+      generate.javascripts false
+      generate.request_specs false
+      generate.routing_specs false
+      generate.stylesheets false
+      generate.test_framework :rspec
+      generate.view_specs false
+    end
+    config.load_defaults 5.1
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
